c6d4a14e982bc6f7021d040fce72e2b2377e8503
Axel Dolce <axel.dolce@amtek.com.ar>
2018-02-15 16:21:14 -0300

    better tests

--- test/index.js
+++ test/index.js
@@ -1,31 +1,63 @@
+var expect = require('chai').expect
+var path = require('path')
+
 var Task = require('../Task')
 
 describe('async-task', function () {
 
     it('should create a task.', async () => {
-        var script = __dirname + '/example-task.js'
+        var script = path.join(__dirname, '/example-task.js')
         var task = Task.instantiate(script)
         var result = await task.execute()
         return task.terminate()
     })
 
     it('should resolve to 42.', async () => {
-        var task = Task.instantiate(__dirname + '/echo-task.js')
-        task.set('result', 42)
+
+        var script = path.join(__dirname, '/echo-task.js')
+        var task = Task.instantiate(script)
+
+        await task.set('result', 42)
         var result = await task.execute()
-        if (result !== 42) throw new Error('Not42')
+
+        expect(result).to.be.equal(42)
+
+    })
+
+    it.only('should execute multiple times.', async () => {
+
+        var script = path.join(__dirname, '/echo-task.js')
+        var task = Task.instantiate(script)
+
+        await task.set('result', 442)
+        var r1 = await task.execute()
+
+        await task.set('result', 33)
+        var r2 = await task.execute()
+
+        task.terminate()
+
+        expect(r1).to.be.equal(442)
+        expect(r2).to.be.equal(33)
+
     })
 
     it('should fail because module does not exist.', async () => {
+
         var failed = false
+
         try {
             var task = await Task.createTaskManager(__dirname + '/idonotexist.js')
         } catch (err) {
             failed = true
         }
-        if (!failed) {
-            throw new Error('ItDidNotFail')
-        }
+
+        expect(failed).to.be.true
+
+    })
+
+    after(() => {
+        //process.exit()
     })
 
 })
